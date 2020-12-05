87970e43a748b8c7b7cb504e9c38447b0b04def7
Andrew <ajuang20@stuy.edu>
2020-12-05 11:03:02 -0500

    Added hasNext and next method

--- Range.java
+++ Range.java
@@ -5,6 +5,7 @@ public class Range implements IntegerSequence{
     public Range(int start,int end){
         this.start = start;
         this.end = end;
+        current = start;
     }
 
     public void reset(){
@@ -16,14 +17,23 @@ public class Range implements IntegerSequence{
     }
     //When current is no longer a valid element in the range, it should return false.
     public boolean hasNext(){
-        
+        if(current<=end){
+            return true;
+        } else {
+            return false;
+        }
     }
-
     //@throws NoSuchElementException when hasNext() is false.
     //This will return the current value, it will also increase current value by 1.
     //e.g.  if current is 5. This will make current 6, and return 5.
     public int next(){
-
+        int value = current;
+        if(!hasNext()){
+            throw new NoSuchElementException();
+        } else {
+            current++;
+            return value;
+        }
     }
 
 }
