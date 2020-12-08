496fa48dabf2ea6ba0d2b934fa1947266736e9ed
Andrew1J <andrewjuang01@gmail.com>
2020-12-08 12:39:10 -0500

    Added constructor, hasNext, next, length and reset methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,8 +1,28 @@
 import java.util.NoSuchElementException;
+
 public class ArraySequence implements IntegerSequence{
     private int currentIndex;
     private int []data;
 
     /*Construct the sequence by copying values from the other array into the data array*/
-    public ArraySequence(int [] other){  }
+    public ArraySequence(int [] other){
+        data = other;
+        currentIndex = 0;
+    }
+
+    public boolean hasNext(){
+        return currentIndex < data.length;
+    }
+
+    public int next(){
+        return data[currentIndex++];
+    }
+
+    public int length(){
+        return data.length;
+    }
+
+    public void reset(){
+        currentIndex = 0;
+    }
 }
