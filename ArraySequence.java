2016dc15212bae6c099e0037c9abbd9df5da2c1d
Andrew1J <andrewjuang01@gmail.com>
2020-12-08 12:46:56 -0500

    Added and tested second constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -9,7 +9,16 @@ public class ArraySequence implements IntegerSequence{
         data = other;
         currentIndex = 0;
     }
-
+    //Postcondition: The otherseq will be reset.
+    //This constructor will copy ALL values of the `otherseq` into the data array.
+    public ArraySequence(IntegerSequence otherseq){
+        data = new int[otherseq.length()];
+        for(int i=0;i<data.length;i++){
+            data[i]=otherseq.next();
+        }
+        otherseq.reset();
+        currentIndex = 0;
+    }
     public boolean hasNext(){
         return currentIndex < data.length;
     }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
     public static void main(String[]args){
+        /*
         IntegerSequence r = new Range(10,15);
         while(r.hasNext()){
             System.out.print(r.next());
@@ -8,6 +9,27 @@ public class Tester{
             }
         }
         System.out.println();
+        */
+        
+        /*part 1
+        int[]nums = {1,3,5,0,-1,3,9};
+        IntegejavarSequence as = new ArraySequence(nums);
+
+        System.out.println("ArraySequence(array):");
+        while(as.hasNext()){
+            System.out.print(as.next()+", ");
+        }
+        */
+
+        //part 2
+        IntegerSequence r = new Range(10,20);
+        IntegerSequence as = new ArraySequence(r);
+
+        System.out.println("ArraySequence(seq):");
+        while(as.hasNext()){
+            System.out.print(as.next()+", ");
+        }
+        System.out.println();
     }
-    
+
 }
