ac088e89797fa6eb010b85ba29c60563748a8e85
Andrew <ajuang20@stuy.edu>
2020-12-23 21:02:41 -0500

    reset sequence in constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -11,6 +11,7 @@ public class ArraySequence implements IntegerSequence{
 
     public ArraySequence(IntegerSequence otherseq){
         data = new int[otherseq.length()];
+        otherseq.reset();
         for(int i=0;i<data.length;i++){
             data[i]=otherseq.next();
         }
